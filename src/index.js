e0922b6b4e3caf8f8610ef3e074eb37ce3314072
59naga <i59naga@icloud.com>
2016-02-22 06:48:30 +0900

    Code refactor

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
 // Dependencies
-import Bluebird from 'bluebird'
+import Bluebird from 'bluebird';
 
-import express from 'express'
-import fsOrigin from 'fs'
-import marked from 'marked'
+import { Router as expressRouter } from 'express';
+import fsOrigin from 'fs';
+import marked from 'marked';
 
 // Module enhancement
-const fs= Bluebird.promisifyAll(fsOrigin)
-const markedAsync= Bluebird.promisify(marked)
+const fs = Bluebird.promisifyAll(fsOrigin);
+const markedAsync = Bluebird.promisify(marked);
 
 // Public
-export default (cwd)=>{
-  const router= express.Router()
+export default (cwd) => {
+  const router = expressRouter();
 
-  router.use((req,res,next)=>{
-    let filePath= req.url.slice(1)
-    if(filePath==='' || filePath.match(/\/$/)){
-      filePath+= 'index'
+  router.use((req, res, next) => {
+    let filePath = req.url.slice(1);
+    if (filePath === '' || filePath.match(/\/$/)) {
+      filePath += 'index';
     }
-    const fileName= `${cwd}/${filePath}.md`
-    const cacheName= `${cwd}/${filePath}.html`
+    const fileName = `${cwd}/${filePath}.md`;
+    const cacheName = `${cwd}/${filePath}.html`;
 
-    const notFound= fs.existsSync(fileName)===false
-    const useCache= fs.existsSync(cacheName)
-    if(notFound){
-      return next()
+    const notFound = fs.existsSync(fileName) === false;
+    const useCache = fs.existsSync(cacheName);
+    if (notFound) {
+      return next();
     }
-    if(useCache){
-      return res.sendFile(cacheName)
+    if (useCache) {
+      return res.sendFile(cacheName);
     }
 
-    fs.readFileAsync(fileName)
-    .then((data)=>(
-      markedAsync(data.toString())
-    ))
-    .then((cache)=>{
-      cache= cache.trim()// 末尾"\n"の削除
-      
-      return fs.writeFileAsync(cacheName,cache)
-      .then((result)=>{
-        res.set('content-type','text/html')
-        res.end(cache)
-      })
-    })
-  })
+    console.log('以降の処理は重いので１度だけ実行したい');
 
-  return router
-}
+    return fs.readFileAsync(fileName)
+    .then((data) => markedAsync(data.toString()))
+    .then((cache) => {
+      const trimedCache = cache.trim();// 末尾"\n"の削除
+
+      return fs.writeFileAsync(cacheName, trimedCache)
+      .then(() => {
+        res.set('content-type', 'text/html');
+        res.end(trimedCache);
+      });
+    });
+  });
+
+  return router;
+};
--- test/index.js
+++ test/index.js
@@ -1,37 +1,43 @@
 // Dependencies
-import middleware from '../src'
+import middleware from '../src';
 
-import express from 'express'
-import caravan from 'caravan'
-import assert from 'power-assert'
-import del from 'del'
+import express from 'express';
+import caravan from 'caravan';
+import assert from 'power-assert';
+import del from 'del';
 
 // Environment
-const cwd= `${__dirname}/fixtures/public`
-const port= 59798
+const cwd = `${__dirname}/fixtures/public`;
+const port = process.env.PORT || 59798;
 
 // Specs
-describe('markedown compile server',()=>{
-  let server
-  before((done)=>{
-    const app= express()
-    app.use('/articles',middleware(cwd))
+describe('markedown compile server', () => {
+  let server;
+  before((done) => {
+    const app = express();
+    app.use('/articles', middleware(cwd));
 
-    server= app.listen(port,done)
-  })
-  after((done)=>{
-    server.close(()=>{
-      del(`${cwd}/**/*.html`).then(()=> done())
+    server = app.listen(port, done);
+  });
+  after((done) => (
+    server.close(() => {
+      del(`${cwd}/**/*.html`).then(() => done());
     })
-  })
+  ));
 
-  it('markdownをhtmlとしてコンパイル返す。htmlはキャッシュとして保存し、以降はコンパイルしない',()=>{
-    return caravan(`http://localhost:${port}/articles`)
-    .progress((progress)=>{
-      assert.equal(progress.value,'<p><strong>要反省である</strong></p>')
+  it('markdownをhtmlとしてコンパイル返す。htmlはキャッシュとして保存し、以降はコンパイルしない', () => {
+    const concurrency = 100;
+    const urls = [];
+    for (let i = 0; i < concurrency; i++) {
+      urls.push(`http://localhost:${port}/articles`);
+    }
+
+    return caravan(urls, { concurrency })
+    .progress((progress) => {
+      assert.equal(progress.value, '<p><strong>要反省である</strong></p>');
     })
-    .then((responses)=>{
-      assert.equal(responses.length,1)
-    })
-  })
-})
+    .then((responses) => {
+      assert.equal(responses.length, concurrency);
+    });
+  });
+});
